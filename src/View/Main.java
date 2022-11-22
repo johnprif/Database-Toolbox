d61dd447612a4c572f96bd2426549cd4f0d474e2
johnprif <56134761+johnprif@users.noreply.github.com>
2022-11-22 12:44:15 +0200

    update version -> v3.0.3

--- src/View/Main.java
+++ src/View/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application
 	private DataBaseHandler myDB;
 	private String path;
 	private PathHandler pathHandler;
-	private String version = " -> v3.0.2";
+	private String version = " -> v3.0.3";
 	private String programTitle = "SAITEC"+version;
 	
 	private Button openOrdersButton;
