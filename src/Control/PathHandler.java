ef123692c4b4f8bf75f022969f2fb749600cad44
johnprif <56134761+johnprif@users.noreply.github.com>
2023-05-09 22:44:48 +0300

    change the scanner with BufferedReader

--- src/Control/PathHandler.java
+++ src/Control/PathHandler.java
@@ -1,11 +1,14 @@
 package Control;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 import Model.DataBaseHandler;
@@ -112,19 +115,36 @@ public class PathHandler implements EventHandler<ActionEvent>
 		}
 	}
 	
-	private void readPath()
-	{
-		try {
-			Scanner myReader = new Scanner(myObj);
-			while (myReader.hasNextLine()) 
-			{
-		        path = myReader.nextLine();
-			}
-			myReader.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+//	private void readPath2()
+//	{
+//		try {
+//			Scanner myReader = new Scanner(myObj);
+//			while (myReader.hasNextLine()) 
+//			{
+//		        path = myReader.nextLine();
+//			}
+//			myReader.close();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+	public void readPath() 
+	{	        
+	        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("databaseLocation.txt"), "UTF-8"))) {
+	            String line;
+	            
+	            while ((line = reader.readLine()) != null) 
+	            {
+	            	path = line;
+	                System.out.println(line);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    
 	}
+
 	
 	private void warningWindow()
 	{
